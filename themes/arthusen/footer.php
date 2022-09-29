1070378cc55f0d2e69ec5e8c57172269f4dc275d
JOHANNA AXHUSEN <93868214+jaxhusen@users.noreply.github.com>
2022-09-29 13:24:17 +0200

    Start: startsida

--- themes/arthusen/footer.php
+++ themes/arthusen/footer.php
@@ -4,13 +4,15 @@
         <p class="footer-navigate">Navigate</p>
         <p class="footer-information">Information</p>
     </h2>
+    <h1 class="footer-links">
+
+    </h1>
 
 
 
 
-    <h1 class="footer-text"> 2022 © ARThusen_ <?php echo date("Y"); ?> All rights reserved. </h1>
+<!--     <h1 class="footer-text"> 2022 © ARThusen_ <?php echo date("Y"); ?> All rights reserved. </h1> -->
 </div>
 
 </body>
-
 </html>
