629a221a839e4ae33ab1a0aff4bd431c8bb2843d
JOHANNA AXHUSEN <93868214+jaxhusen@users.noreply.github.com>
2022-10-05 17:14:46 +0200

    blä

--- themes/arthusen/page.php
+++ themes/arthusen/page.php
@@ -1,15 +1,27 @@
 <?php get_header(); ?>
 
+
 <div class="content">
 
     <img src="<?php the_post_thumbnail_url("post_image") ?>" alt="">
 
 
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+   <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
         <?php the_content() ?>
     <?php endwhile; else: endif; ?>
 </div>
 
 
 
-<?php get_footer(); ?>
+
+<!-- Ifall de är första sidan så visar den nyheterna -->
+<?php if (is_search_page()) : ?>
+<div>
+<?php get_template_part('template-parts/search.php'); ?>
+</div>
+<?php endif; ?>
+
+
+
+<?php
+get_footer();
