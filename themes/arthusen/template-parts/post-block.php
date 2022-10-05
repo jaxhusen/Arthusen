136afa91fbb55cb861dc8e949a8051ab5e28c2ca
Julia Kleresjö <92084007+kleresjo@users.noreply.github.com>
2022-10-05 21:32:31 +0200

    style

--- themes/arthusen/template-parts/post-block.php
+++ themes/arthusen/template-parts/post-block.php
@@ -1,12 +1,14 @@
-<div class="post-block">
-<div class="post-box">
 
+
+
+<div class="post-block">
+<span class="post-box">
  <?php the_post_thumbnail('medium'); ?>
  <a href="<?php the_permalink(); ?>" class="news-title"> <?php the_title(); ?> </a>
-<div class="excerpt-block">
+<span class="excerpt-block">
     <?php the_excerpt(); ?>
     <a href="<?php the_permalink(); ?>" class="read-title"> Läs mer </a>
 
-</div>
+</span>
 </div>
 
