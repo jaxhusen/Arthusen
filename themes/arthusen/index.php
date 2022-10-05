6995a10b2ff7c31db68d9825546ed49a1fd38619
Julia Kleresjö <92084007+kleresjo@users.noreply.github.com>
2022-10-05 16:56:26 +0200

    jättemkt styling tack o hej

--- themes/arthusen/index.php
+++ themes/arthusen/index.php
@@ -1,17 +1,16 @@
 <?php
 get_header();
 ?>
+
 <div class="index-content">
-
-
     <?php if(have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
         <?php get_template_part('template-parts/post-block'); ?>
         <?php endwhile; ?>
         <?php wp_reset_postdata(); ?>
         <?php endif; ?>
-        
 </div>
+
 <?php
 get_footer();
 
--- themes/arthusen/page.php
+++ themes/arthusen/page.php
@@ -1,15 +1,11 @@
 <?php get_header(); ?>
 
 <div class="content">
-
     <img src="<?php the_post_thumbnail_url("post_image") ?>" alt="">
-
-
     <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
         <?php the_content() ?>
     <?php endwhile; else: endif; ?>
 </div>
-
 
 
 <?php get_footer(); ?>
