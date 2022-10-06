a74fe174d1d2e829f42bb36112130c3468412153
Julia Kleresjö <92084007+kleresjo@users.noreply.github.com>
2022-10-06 09:38:32 +0200

    style

--- themes/arthusen/woocommerce.php
+++ themes/arthusen/woocommerce.php
@@ -1,25 +1,19 @@
-hej wocoommerce.php
+hej wocoommerce.php 
 <?php
 get_header();
 ?>
-
-<h1>Mina sidor</h1>
-<h3>Orderhistorik</h3>
-
-<div class="endiv">
-<?php woocommerce_content(); ?>
 
 
     <?php
     if (is_product_category()) {
     ?>
-        <h1 class="omh1">Show categories</h1>
-
+        
+       
     <?php
     } ?>
 <?php woocommerce_content(); ?>
 
-</div>
+
 
 <?php get_footer(); ?>
 
